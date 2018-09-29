d989f73b7cd2f481b306a6b5b4179b71a818aa53
Andrew Wright <naruhodo007@gmail.com>
2018-09-29 08:45:31 +0000

    Update & Working Versin

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,29 +1,28 @@
 import requests
-import sqlite3
 
-QUERY = '''INSERT INTO gists (
-                        github_id, html_url, git_pull_url, git_push_url, 
-                        commits_url, forks_url, public, created_at, updated_at, 
-                        comments, comments_url)
-                VALUES (                  
-                        :github_id, :html_url, :git_pull_url, :git_push_url, 
-                        :commits_url, :forks_url, :public, :created_at, 
-                        :updated_at, :comments, :comments_url
-                        );'''
-                        
+
+GIST_QUERY = """INSERT INTO gists (
+    "github_id", "html_url", "git_pull_url",
+    "git_push_url", "commits_url",
+    "forks_url", "public", "created_at",
+    "updated_at", "comments", "comments_url"
+) VALUES (
+    :github_id, :html_url, :git_pull_url,
+    :git_push_url, :commits_url, :forks_url,
+    :public, :created_at, :updated_at,
+    :comments, :comments_url
+);"""
+
 URL = 'https://api.github.com/users/{username}/gists'
 
 
 def import_gists_to_database(db, username, commit=True):
+    resp = requests.get(URL.format(username=username))
+    resp.raise_for_status()
 
-    response = requests.get(URL.format(username=username))
-
-    response.raise_for_status()
-    gist_dump = response.json()
-
-
-    for gist in gist_dump:
-        gist_params = {
+    gists_data = resp.json()
+    for gist in gists_data:
+        params = {
             "github_id": gist['id'],
             "html_url": gist['html_url'],
             "git_pull_url": gist['git_pull_url'],
@@ -36,8 +35,6 @@ def import_gists_to_database(db, username, commit=True):
             "comments": gist['comments'],
             "comments_url": gist['comments_url'],
         }
-        
-        db.execute(QUERY, gist_params)
-        
+        db.execute(GIST_QUERY, params)
         if commit:
             db.commit()
