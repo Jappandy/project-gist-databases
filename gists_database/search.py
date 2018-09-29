2f977598e9ef695c51f24ee6e74c6e5da4740f28
Andrew Wright <naruhodo007@gmail.com>
2018-09-29 06:42:31 +0000

    Passed all tests

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,27 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    query = 'SELECT * FROM gists'
+    
+    result = []
+    params = {}
+    
+    for name, value in kwargs.items():
+        if name == 'created_at':
+            git_name = name
+            query += ' WHERE datetime({git_name}) = datetime(:date) AND'.format(
+                                                                    git_name)
+            params.update({'date' : value})
+       
+        else:
+            git_name = name
+            query += ' WHERE {git_name} = :value AND'.format(git_name)
+            params.update({'value' : value})
+    
+    query = query.rstrip(' AND')
+    cursor = db_connection.execute(query, params)
+        
+    for gist in cursor:
+        result.append(Gist(gist))
+    
+        return result
